e345736a8f1fb7f9ac127b446a3a07e46c0275dc
Shane Nicholson <shanescottnicholson@gmail.com>
2021-10-06 11:18:02 -0700

    Adds hash compatibility to class Item

--- store_item.rb
+++ store_item.rb
@@ -1,29 +1,29 @@
 inventory = [
-  { item: "Safe", color: "Silver", price: 1200 },
-  { item: "Air Conditioner", color: "White", price: 350 },
-  { item: "Trampoline", color: "Blue", price: 800 },
-  { item: "Tin Can", color: "Grey", price: 0.5 },
+  { item_name: "Safe", item_color: "Silver", item_price: 1200 },
+  { item_name: "Air Conditioner", item_color: "White", item_price: 350 },
+  { item_name: "Trampoline", item_color: "Blue", item_price: 800 },
+  { item_name: "Tin Can", item_color: "Grey", item_price: 0.5 },
 ]
 
 class Item
   attr_reader :item_name, :item_color, :item_price
   attr_writer :item_name, :item_color, :item_price
 
-  def initialize(item_name, item_color, item_price)
-    @item_name = item_name
-    @item_color = item_color
-    @item_price = item_price
+  def initialize(input_hash)
+    @item_name = input_hash[:item_name]
+    @item_color = input_hash[:item_color]
+    @item_price = input_hash[:item_price]
   end
 
   def item_info
-    puts "#{item_name} is #{item_color} and costs #{@item_price} dollars."
+    puts "#{@item_name} is #{@item_color} and costs #{@item_price} dollars."
   end
 end
 
-item = Item.new("Tin Can", "Grey", 0.5)
-item2 = Item.new("Trampoline", "Blue", 800)
-item3 = Item.new("Air Conditioner", "White", 350)
-item4 = Item.new("Stick", "Woody", 4)
+item = Item.new(inventory[0])
+item2 = Item.new(inventory[1])
+item3 = Item.new(inventory[2])
+item4 = Item.new(inventory[3])
 
 puts item.item_info
 puts item2.item_info
