a138792ab5c3b79ea4ad34fbdd34b3b9b13262cd
Shane Nicholson <shanescottnicholson@gmail.com>
2021-10-06 13:58:50 -0700

    Bugfixed exercise 5 in bugfix.rb

--- bugfix.rb
+++ bugfix.rb
@@ -32,21 +32,65 @@
 #   puts "That's the future!"
 # end
 
-class Person
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
-  end
+# class Person
+#   def initialize(first_name, last_name)
+#     @first_name = first_name
+#     @last_name = last_name
+#   end
 
-  def first_name
-    @first_name
-  end
+#   def first_name
+#     @first_name
+#   end
 
-  def to_s
-    puts "#{@first_name} #{@last_name}"
+#   def to_s
+#     puts "#{@first_name} #{@last_name}"
+#   end
+# end
+
+# tj = Person.new("Thomas", "Jefferson")
+# puts tj.first_name
+# puts tj.to_s
+
+puts "Input exam grade one:"
+exam_one = gets.chomp.to_i
+
+puts "Input exam grade two:"
+exam_two = gets.chomp.to_i
+
+puts "Input exam grade three:"
+exam_three = gets.chomp.to_i
+
+def list_grade(exam_one, exam_two, exam_three)
+  puts "Exams: #{exam_one}, #{exam_two}, #{exam_three}"
+end
+
+def average_grade(exam_one, exam_two, exam_three)
+  @average = (exam_one + exam_two + exam_three / 3).to_i
+end
+
+def letter_grade(average_grade)
+  if average_grade <= 59
+    puts "Grade: F"
+  elsif average_grade >= 60 && average_grade <= 69
+    puts "Grade: D"
+  elsif average_grade >= 70 && average_grade <= 79
+    puts "Grade: C"
+  elsif average_grade >= 80 && average_grade <= 89
+    puts "Grade: B"
+  elsif average_grade >= 90
+    puts "Grade: A"
   end
 end
 
-tj = Person.new("Thomas", "Jefferson")
-puts tj.first_name
-puts tj.to_s
+def pass_fail(average)
+  if @average < 59
+    puts "Student is failing."
+  else
+    puts "Student is passing."
+  end
+end
+
+list_grade(exam_one, exam_two, exam_three)
+puts "Average: #{average_grade(exam_one, exam_two, exam_three)}"
+letter_grade(@average)
+pass_fail(@average)
