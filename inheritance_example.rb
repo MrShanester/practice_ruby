24951d9afdf4a8bc90cb15d557a58f74b6fdea43
Shane Nicholson <shanescottnicholson@gmail.com>
2021-10-06 12:14:52 -0700

    Adds DRY attributes to Bike and Car classes

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -18,12 +18,27 @@ class Vehicle
 end
 
 class Car < Vehicle
+  def initialize
+    super
+    @fuel = "Gas"
+    @make = "GMC"
+    @model = "Sierra"
+    @year = "1998"
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
 class Bike < Vehicle
+  def initialize
+    super
+    @type = "Bodfish"
+    @weight = 14
+    @tire = "Road Tread"
+  end
+
   def ring_bell
     puts "Ring ring!"
   end
