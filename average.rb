321a88961ce6cf3eab40089b022aa86f66550f2a
Shane Nicholson <shanescottnicholson@gmail.com>
2021-10-07 13:55:07 -0700

    Add frames

--- average.rb
+++ average.rb
@@ -1,8 +1,20 @@
-1.times do
+frames = []
+
+2.times do
   puts "How many pins were knocked down on the first throw?"
   throw1 = gets.chomp.to_i
   puts "How many pins were knocked down on the second throw?"
   throw2 = gets.chomp.to_i
+
+  if throw1 == 10 || throw2 == 10
+    puts "Strike!"
+  elsif throw1 + throw2 == 10
+    puts "Spare!"
+  end
+
   score = throw1 + throw2
+  frames << score
   puts "Your score it #{score}"
 end
+
+puts frames
